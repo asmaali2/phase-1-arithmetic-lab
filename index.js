4096c414a0c734c93f5a653d6497405600f579d9
asmaali2 <asma.maalim7@gmail.com>
2023-10-05 12:54:43 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 
 // Write your code here
+let num1=31,num2=2
+let multiply=num1*num2;
+
+let random=Math.floor(Math.random()*10)+1;
+
+let max=Math.max(2,4,20,16,13)
+
+let num3=44,num4=10
+let mod =44%10
